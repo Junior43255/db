419fae8c186b3ea4a1fbecf48726291d9613b7d8
Junior <pantakhan.u@gmail.com>
2022-03-28 17:07:57 +0700

    first commit

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ server.use(jsonServer.rewriter({
 }))
 server.use(router);
 server.listen(PORT, () => {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log('Server is running');
 });
